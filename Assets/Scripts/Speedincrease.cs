a1b76cf116b00bfdd363d5ce873817ea8bd543c6
Sam Swoboda <samoswoboda@gmail.com>
2020-02-23 23:07:07 -0600

    speed item

---
+++ Assets/Scripts/Speedincrease.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speedincrease : MonoBehaviour
+{
+    public string type;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>())
+        {
+            Destroy(gameObject);
+            PlayerController.speed = Speedincrease * 2;
+        }
+    }
+}
