d4b60cb988cef66d2526f661488a17397aa47e5e
Shane Blair <sblair@wustl.edu>
2020-04-10 21:40:19 -0400

    Finish Torch persistence logic, FinalDoorController logic

---
+++ Assets/Scripts/FinalDoorController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinalDoorController : MonoBehaviour
+{
+    public GameObject[] torches;
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += CheckAndLightTorches;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= CheckAndLightTorches;
+    }
+
+    public void CheckAndLightTorches(Scene scene, LoadSceneMode mode)
+    {
+        if (PlayerController.instance != null)
+        {
+            for (int i = 0; i < PlayerController.instance.torchesLit; i++)
+            {
+                torches[i].GetComponent<TorchController>().SetLit(true);
+            }
+        }
+    }
+}
--- Assets/Scripts/TorchController.cs
+++ Assets/Scripts/TorchController.cs
@@ -1,28 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TorchController : MonoBehaviour
 {
-    private SpriteRenderer _sr;
-    private Animator animator;
+    private static Dictionary<string, bool> mapTorches = new Dictionary<string, bool>
+    {
+        { "Scene2", false },
+        { "Scene5", false },
+        { "Scene11", false },
+        { "Scene12", false }
+    };
 
+    private Animator animator;
     private bool lit = false;
 
-    private void Start()
+    public bool openingTorch;
+    public bool mainMenuTorch;
+
+    public void SetLit(bool lit)
     {
-        _sr = GetComponent<SpriteRenderer>();
+        this.lit = lit;
+        animator.SetBool("lit", true);
+    }
+
+    private void Awake()
+    {
         animator = GetComponent<Animator>();
+
+        // Check if we have already lit this torch or not
+        string loadedScene = SceneManager.GetActiveScene().name;
+        if (mainMenuTorch || (mapTorches.ContainsKey(loadedScene) && mapTorches[loadedScene]))
+        {
+            SetLit(true);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (!lit)
+        if (!lit && !openingTorch)
         {
             if (LayerMask.LayerToName(collision.gameObject.layer) == PlayerController.PLAYER_PROJECTILE)
             {
-                lit = true;
-                animator.SetBool("lit", true);
+                SetLit(true);
+                mapTorches[SceneManager.GetActiveScene().name] = true;
                 PlayerController.instance.torchesLit += 1;
             }
         }
